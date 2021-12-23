0ac9178726430b7a8f2f78f6181afa8ab9781984
Serj <Assimilator@users.noreply.github.com>
2021-12-23 12:53:38 -0700

    refactored D9 code, added part2 solution using CCL

--- adventofcodeD09.py
+++ adventofcodeD09.py
@@ -2,47 +2,39 @@ import sys
 
 import numpy as np
 import pandas as pd
-from scipy.signal import argrelextrema, argrelmin
+from scipy import ndimage
 
+
+def loadData():
+    lines = open(sys.argv[1], "r").readlines()
+    df = pd.DataFrame()
+    cols = 0
+    for line in lines:
+        df[str(cols)] = list(str(line.strip().replace('\n','')))
+        cols+=1
+    return(df.T.astype(int).to_numpy())
+
+def fMinima(map):
+    return (map == ndimage.minimum_filter(map, 3, mode='constant', cval=9))
+        
+def part1(map):
+    mins = fMinima(map)
+    print(np.sum(np.where( mins == True, map+1, 0)))
+
+def part2(map):
+
+    # CCL by zeroed ridges, then flattening into 1d
+    basins = ndimage.label(np.where((map == 9), 0, 1 ))[0].flatten()
+    
+    # Getting 3 largest non-zero objects
+    lBasins = np.sort(np.unique((np.take(basins,np.where(basins != 0))[0]), return_counts=True)[1])[-3:]
+    print(np.prod(lBasins))
 
 def main():
-    mapDF = pd.read_csv(sys.argv[1], header=None, names=['Col1'])
-    mapDF['Col'] =  mapDF['Col1'].astype(str)
-    mapDF.drop('Col1', axis=1, inplace=True)
-    print(len(mapDF['Col'][0]))
-    for i in range(len(mapDF['Col'][0])):
-        mapDF['Col'+str(i)] = mapDF['Col'].astype(str).str[i:i+1]
-    mapDF.drop('Col', axis=1, inplace=True)
-    mapDF = mapDF.astype(int)#.T
-    print(mapDF)
-    print(pd.DataFrame.idxmin(mapDF))
-    map = mapDF.astype(int).to_numpy()
-    print(map)
-    # print((map.flatten('F')))
-    print()
-   
-    # print(np.array(argrelextrema(map, np.less)))
-    # print(np.array(argrelextrema(map, np.greater)))
-    
-    localMinima = [0] * (0)
-    # print(localMinima,[(np.r_[True, map[1:] < map[:-1]] & np.r_[map[:-1] < map[1:], True])])
-    for i in range(len(map)):
-        line = map[i]
-        localMinima = localMinima.append((np.r_[True, line[1:] < line[:-1]] & np.r_[line[:-1] < line[1:], True]))
-        # print()
-        print(localMinima)
-    
-    mapT = mapDF.astype(int).T.to_numpy()
-    print(mapT)
-    
-    for i in range(len(mapT)):
-        line = mapT[i]
-        print(np.r_[True, line[1:] < line[:-1]] & np.r_[line[:-1] < line[1:], True])
-    
-    # print(s[1:-1][s[1:-1] < s[:-2] and s[1:-1] < s[2:]])
-    # print(np.argmin(map[0]))
-    # print(np.argmin(map, axis=0))
-    # print(np.argmin(map, axis=1))
+    map = loadData()
+    # print(map)
+    part1(map)
+    part2(map)
 
 if __name__ == '__main__':
     main()
