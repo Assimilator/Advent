30bf682adf8d8f6f9680cb8ef49437437e4b27d8
Serj <Assimilator@users.noreply.github.com>
2022-01-27 13:26:25 -0700

    Refactored D13P1, added P2 solution

--- 2021/adventofcodeD13.py
+++ 2021/adventofcodeD13.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+from scipy import ndimage
 
 
 def loadData():
@@ -11,8 +12,8 @@ def loadData():
         if ',' in line:
             indices.append([int(index) for index in line.strip().replace('\n','').split(',')])
         elif '=' in line:
-            #x and y are converted into axis values
-            folds.append([int(fold) for fold in line.strip().replace('\n','').replace('fold along ','').replace('y','0').replace('x','1').split('=')])
+            # x and y are converted into axis values
+            folds.append([int(fold) for fold in line.strip().replace('\n','').replace('fold along ','').replace('y','1').replace('x','0').split('=')])
     return indices, folds
 
 def drawSheet(indices):
@@ -21,24 +22,41 @@ def drawSheet(indices):
         sheet[index[0]][index[1]] += 1
     return sheet
 
-#TODO: complete the method
-def foldSheet(sheet, folds):
-    foldedSheet = []
+def foldSheet(sheet, folds, foldOnce = 0):
     if len(folds) > 0:
+        foldedSheet = []
         fold = folds.pop(0)
-        if fold[0] == 0:
-            foldedSheet = sheet[:,:fold[1]] + np.flip(sheet[:,fold[1]+1:], axis=1)
+        
+        if fold[0] == 1:
+            # Vertical fold
+            foldedSheet = sheet[:,:fold[1]] + np.flip(sheet[:,fold[1]+1:], axis=fold[0])
         else:
-            foldedSheet = sheet[:fold[1],:] + np.flip(sheet[fold[1]+1:,:], axis=0)
-        return foldedSheet #foldSheet(foldedSheet.copy(), folds)   
+            # Horizontal fold
+            foldedSheet = sheet[:fold[1],:] + np.flip(sheet[fold[1]+1:,:], axis=fold[0])
+        
+        if foldOnce == 1:
+            # Part1 solution
+            return foldedSheet 
+        else:
+            # Part2 solution
+            return foldSheet(foldedSheet.copy(), folds)
     else:
         return sheet
 
 def main():
+    
     indices, folds = loadData()
     sheet = drawSheet(indices)
-    foldedSheet = foldSheet(sheet.copy(), folds.copy())
+    
+    # Part1
+    foldedSheet = foldSheet(sheet.copy(), folds.copy(), 1)
     print(np.sum(np.count_nonzero(foldedSheet)))
+    
+    # Part2 
+    foldedSheet = foldSheet(sheet.copy(), folds.copy())
+    image = np.flip((np.where((foldedSheet > 0), '#', ' ' )), axis=0)
+    np.set_printoptions(threshold=sys.maxsize)
+    print(image)
 
 if __name__ == '__main__':
     main()
