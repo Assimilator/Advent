d5074d5c46bfe37226a5e0d9c7dce9bc3ad5fed5
Serj <Assimilator@users.noreply.github.com>
2022-04-24 04:09:00 -0600

    Major refactor, tests, .gitignore

--- 2021/adventofcodeD11.py
+++ 2021/adventofcodeD11.py
@@ -1,54 +1,66 @@
 import sys
+
 import numpy as np
 import pandas as pd
 from scipy import ndimage
 
+if len(sys.argv) > 1:
+    file_name = sys.argv[1]
+else:
+    file_name = ""
 
-def loadData():
-    lines = open(sys.argv[1], "r").readlines()
+
+def load_data(file):
+    input_file = open(file, "r")
+    lines = input_file.readlines()
+    input_file.close()
     df = pd.DataFrame()
     cols = 0
     for line in lines:
-        df[str(cols)] = list(str(line.strip().replace('\n','')))
-        cols+=1
-    return(df.T.astype(int).to_numpy())
+        df[str(cols)] = list(str(line.strip().replace('\n', '')))
+        cols += 1
+    return df.T.astype(int).to_numpy()
 
-def processFlashes(grid, flashes, flashCount):
-    newFlashes = flashes + np.where((grid > 9), 1, 0 )
-    newFlashes = np.where(newFlashes > 0, 1, 0)
-    newFlashCount = np.sum(newFlashes)
-    if newFlashCount > flashCount:
-        grid += ndimage.correlate(np.subtract(newFlashes, flashes), np.ones(9).reshape(3,3), mode='constant', cval=0)
-        return processFlashes(grid, newFlashes, newFlashCount)
+
+def process_flashes(grid, flashes, flash_count):
+    new_flashes = flashes + np.where((grid > 9), 1, 0)
+    new_flashes = np.where(new_flashes > 0, 1, 0)
+    new_flash_count = np.sum(new_flashes)
+    if new_flash_count > flash_count:
+        grid += ndimage.correlate(np.subtract(new_flashes, flashes), np.ones(9).reshape(3, 3), mode='constant', cval=0)
+        return process_flashes(grid, new_flashes, new_flash_count)
     else:
-        return [np.where(grid > 9, 0, grid), flashCount]
+        return [np.where(grid > 9, 0, grid), flash_count]
+
 
 def part1(grid):
-    flashCount = 0    
+    flash_count = 0
     for i in range(100):
         grid += 1
-        flashed = processFlashes(grid, np.zeros(grid.shape, dtype=int), 0)
+        flashed = process_flashes(grid, np.zeros(grid.shape, dtype=int), 0)
         grid = flashed[0]
-        flashCount += flashed[1]
-    print(flashCount)
+        flash_count += flashed[1]
+    return flash_count
+
 
 def part2(grid):
-    newFlashCount = 0
+    new_flash_count = 0
     step = 1
-    maxFlashCount = grid.shape[0]*grid.shape[1]
-    while newFlashCount < maxFlashCount:
+    max_flash_count = grid.shape[0] * grid.shape[1]
+    while new_flash_count < max_flash_count:
         step += 1
         grid += 1
-        flashed = processFlashes(grid, np.zeros(grid.shape, dtype=int), 0)
+        flashed = process_flashes(grid, np.zeros(grid.shape, dtype=int), 0)
         grid = flashed[0]
-        newFlashCount = flashed[1]
-    print(step)
+        new_flash_count = flashed[1]
+    return step
+
 
 def main():
-    grid = loadData()
-    test = np.zeros(grid.shape, dtype=int)
-    part1(grid)
-    part2(grid)
+    grid = load_data(file_name)
+    print(part1(grid))
+    print(part2(grid))
+
 
 if __name__ == '__main__':
     main()
--- 2021/adventofcodeD12.py
+++ 2021/adventofcodeD12.py
@@ -1,45 +1,53 @@
 import sys
 
+if len(sys.argv) > 1:
+    file_name = sys.argv[1]
+else:
+    file_name = ""
 
-def loadData():
-    inputFile = open(sys.argv[1], "r")
-    lines=inputFile.readlines()
+
+def load_data(file):
+    input_file = open(file, "r")
+    lines = input_file.readlines()
+    input_file.close()
     links = []
     for line in lines:
-        links.append(line.strip().replace('\n','').split('-'))
+        links.append(line.strip().replace('\n', '').split('-'))
     return links
 
-def processNodes(node, usedNodes, links, extraPass = 0, usedNode = ''):
-    children = getChildren(node, links)
+
+def process_nodes(node, used_nodes, links, extra_pass=0, used_node=''):
+    children = get_children(node, links)
     ends = 0
     if len(children) == 0:
         return 0
     if 'end' in children:
         ends += 1
     if node.islower():
-        usedNodes.append(node)
+        used_nodes.append(node)
     for child in children:
         if child not in ['start', 'end']:
-            if child not in usedNodes:
-                ends += processNodes(child, usedNodes.copy(), links, extraPass, usedNode)
-            elif extraPass > 0 and usedNode == '':
-                ends += processNodes(child, usedNodes.copy(), links, extraPass, child)
+            if child not in used_nodes:
+                ends += process_nodes(child, used_nodes.copy(), links, extra_pass, used_node)
+            elif extra_pass > 0 and used_node == '':
+                ends += process_nodes(child, used_nodes.copy(), links, extra_pass, child)
     return ends
 
-def getChildren(node, links):
+
+def get_children(node, links):
     children = []
     for link in links:
         if node in link:
             children.extend(link)
-    children = list(filter((node).__ne__, children))
+    children = list(filter(node.__ne__, children))
     return children
 
+
 def main():
-    links = loadData()
-    count = processNodes('start', [], links)
-    print(count)
-    count = processNodes('start', [], links, 1, '')
-    print(count)
+    links = load_data(file_name)
+    print(process_nodes('start', [], links))
+    print(process_nodes('start', [], links, 1, ''))
+
 
 if __name__ == '__main__':
     main()
